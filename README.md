8f8b189f6be22ffa3e4173f3b241c763b2f12355
Ray Myers <ray.myers@gmail.com>
2010-03-15 20:45:11 -0500

    Updated README and clarified license.

---
+++ README.md
@@ -0,0 +1,23 @@
+# JHaml
+
+A Java implementaion of Haml (XHTML Abstraction Markup Language). Work in progress.
+
+Haml features not currently supported:
+
+* Interpolation.
+
+* External languages like MarkDown and Sass.
+
+* Ugly mode for faster rendering speed.
+
+## Authors
+
+JHaml was created by [Ray Myers](http://cadrlife.com), with very special thanks to the implementers of the 
+original Haml, [Hampton Catlin](http://hamptoncatlin.com) and [Nathan Weizenbaum](http://nex-3.com).
+
+This implementation is licensed under the GNU GPL version 3.0, with the exception of the included 
+libraries, which are licensed as specified in their source folders.
+
+Antlr: BSD
+Google Guava: Apache 2.0
+Apache Commons Lang: Apache 2.0
--- src/com/cadrlife/jaml/JamlConfig.java
+++ src/com/cadrlife/jaml/JamlConfig.java
@@ -19,6 +19,7 @@ public class JamlConfig {
 	List<String> autoclose = new ArrayList<String>(Arrays.asList("meta", "img", "link", "br", "hr", "input", "area", "param", "col", "base"));
 	List<String> preserve = new ArrayList<String>(Arrays.asList("textarea", "pre"));
 	public String attrWrapper = "'";
+	
 	public Map<String,Filter> filters = new HashMap<String,Filter>();
 	{
 		filters.put("plain", new PlainFilter(this));
@@ -29,6 +30,12 @@ public class JamlConfig {
 		filters.put("jsp", new PlainFilter(this));
 		filters.put("css", new CssFilter(this));
 	}
+	
+	// These options escapeHtml, suppressEval, and encoding may not be necessary.
+//	boolean escapeHtml = false;
+//	boolean suppressEval = false;
+//	String encoding = "utf-8";
+	
 	public boolean isXhtml() {
 		return !isHtml();
 	}
