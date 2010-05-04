5f9b6b5e902ed236a5e16dedbe96158e53aab03c
Ray Myers <ray.myers@gmail.com>
2010-05-04 18:18:35 -0500

    Add batch converter.

---
+++ src/com/cadrlife/jhaml/JHamlBatchConverter.java
@@ -0,0 +1,75 @@
+package com.cadrlife.jhaml;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.charset.Charset;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+import java.util.regex.Pattern;
+
+import com.cadrlife.jhaml.internal.com.google.common.io.Files;
+import com.cadrlife.jhaml.internal.util.DirectoryWalker;
+
+public class JHamlBatchConverter extends DirectoryWalker {
+	private boolean deleteHamlFiles = false;
+	private String hamlExtension = "haml";
+	private String targetExtenstion = "jsp";
+	private Charset charset = Charset.defaultCharset();
+	public List<File> convertAllInPath(File startDirectory) throws IOException {
+		List<File> results = new ArrayList<File>();
+		walk(startDirectory, results);
+		return results;
+	}
+	
+
+	@Override
+	protected void handleFile(File file, Collection<File> results) throws IOException {
+		if (file.getName().endsWith("." + hamlExtension)) {
+				String gsp = new JHaml()
+						.parse(Files.toString(file, getCharset()));
+				String gspFileName = file.getAbsolutePath().replaceAll(
+						"\\." + Pattern.quote(hamlExtension) + "$", "." + targetExtenstion);
+				Files.write(gsp, new File(gspFileName), getCharset());
+			if (isDeleteHamlFiles()) {
+				file.delete();
+			}
+		}
+		results.add(file);
+	}
+
+	public void setDeleteHamlFiles(boolean deleteHamlFiles) {
+		this.deleteHamlFiles = deleteHamlFiles;
+	}
+
+	public boolean isDeleteHamlFiles() {
+		return deleteHamlFiles;
+	}
+
+	public void setTargetExtenstion(String targetExtenstion) {
+		this.targetExtenstion = targetExtenstion;
+	}
+
+	public String getTargetExtenstion() {
+		return targetExtenstion;
+	}
+
+	public void setHamlExtension(String hamlExtension) {
+		this.hamlExtension = hamlExtension;
+	}
+
+	public String getHamlExtension() {
+		return hamlExtension;
+	}
+
+
+	public void setCharset(Charset charset) {
+		this.charset = charset;
+	}
+
+
+	public Charset getCharset() {
+		return charset;
+	}
+
+}
---
+++ src/com/cadrlife/jhaml/internal/util/DirectoryWalker.java
@@ -0,0 +1,33 @@
+package com.cadrlife.jhaml.internal.util;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.Collection;
+
+public abstract class DirectoryWalker {
+
+	public DirectoryWalker() {
+		super();
+	}
+
+	protected abstract void handleFile(File file, Collection<File> results)
+	throws IOException;
+
+	protected void walk(File root, Collection<File> results) throws IOException {
+		root = root.getCanonicalFile();
+		if (root.exists()) {
+			if (root.isFile()) {
+				handleFile(root,  results);
+			} else {
+				File[] children = root.listFiles();
+				for (File child : children) {
+					walk(child, results);
+				}
+			}
+		} else {
+			throw new FileNotFoundException(root.getCanonicalPath());
+		}
+	}
+
+}
